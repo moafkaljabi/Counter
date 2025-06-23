b47423c2b37987e026f62b3d8130387867501ab2
Moafk Aljabi <mwafak_sh@hotmail.com>
2025-06-24 01:38:13 +0200

    Working on the testbench

--- Counter.srcs/sim_1/new/Counter_TB.vhd
+++ Counter.srcs/sim_1/new/Counter_TB.vhd
@@ -21,23 +21,38 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity Counter_TB is
---  Port ( );
+entity Counter_TB is 
 end Counter_TB;
 
-architecture Behavioral of Counter_TB is
+
+architecture behavior of Countet_TB is
+
+    -- Component Under Test
+    component Counter
+        generic (
+            MAX_COUNT:  integer := 9
+        );
+
+        port (
+            clk     : in std_logic;
+            nRst    : in std_logic;
+            enable  : in std_logic;
+
+            done    : out std_logic;
+            count   : out integer range 0 to MAX_COUNT;
+        );
+    end component;
+
+    -- Signals to connect to the CUT
+    signal clk_tb       : std_logic := '0';
+    signal nRst_tb      : std_logic := '0';
+    signal enable_tb    : std_logic := '0';
+
+    signal done_tb      : std_logic;
+    signal count_tb     : integer range 0 to 9;
 
 begin
 
-
-end Behavioral;
+end behavior;
