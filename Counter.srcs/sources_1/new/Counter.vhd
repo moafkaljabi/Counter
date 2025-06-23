da9efa9def2cba33d83fee86ccdf49d17b700045
Moafk Aljabi <mwafak_sh@hotmail.com>
2025-06-23 22:48:27 +0200

    Started repo

---
+++ Counter.srcs/sources_1/new/Counter.vhd
@@ -0,0 +1,33 @@
+----------------------------------------------------------------------------------
+-- Company: Astraspecs  
+-- Engineer: Moafk Aljabi
+-- 
+-- Create Date: 06/23/2025 07:46:15 PM
+-- Design Name: 
+-- Module Name: Counter - Behavioral
+-- Project Name: Counter
+-- Target Devices: Kria SoM
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Counter is
+--  Port ( );
+end Counter;
+
+architecture Behavioral of Counter is
+
+begin
+
+end Behavioral;
